621cde2d64c2f89f4d3aeb5817c660c5bd951181
SitiNuriaR <nuriarachma1106@gmail.com>
2017-11-06 10:30:27 +0700

    171106

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -36,4 +37,15 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+	
+	public function userLogout()
+	{
+	Auth::guard('web')->logout();
+	return redirect('/');
+	}
+	
+	public function __construct()
+	{
+	$this->middleware('guest', ['except' => ['logout', 'userLogout']]);
+	}
 }
