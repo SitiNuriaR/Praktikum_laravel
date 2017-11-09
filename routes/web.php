e55acfc18ab10f0325005e742519e4b7f9ef275d
SitiNuriaR <nuriarachma1106@gmail.com>
2017-11-09 16:57:37 +0700

    171109

--- routes/web.php
+++ routes/web.php
@@ -59,11 +59,17 @@ Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.logi
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/users/logout', 'Auth\LoginController@userLogout')->name('user.logout');
+
 Route::prefix('admin')->group(function() {
+Route::resource('manageadmins', 'ManageAdminController');	
 Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
 Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 Route::get('/', 'AdminController@index')->name('admin.dashboard');
 Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+
+
+
 });
 
 
+?>
